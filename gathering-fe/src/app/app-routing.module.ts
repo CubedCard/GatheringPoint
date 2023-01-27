16117cb0ae6efe7957e492f6d05c9ada67d333b0
J. Derksen <jip.derksen@hva.nl>
2023-01-27 20:49:36 +0100

    created the frame for the main page

--- gathering-fe/src/app/app-routing.module.ts
+++ gathering-fe/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'feed', component: MainComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' }
 ];
